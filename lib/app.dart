f3146c82181fa09a7255a1e838d5258fd74ab188
LygiaGarrido <lygiagarrido@hotmail.com>
2026-04-23 14:31:50 +0100

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'core/router/routes.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Event Check-in',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(textTheme: GoogleFonts.nunitoTextTheme()),
+      routerConfig: createRouter(),
+    );
+  }
+}
---
+++ lib/core/router/routes.dart
@@ -0,0 +1,8 @@
+import 'package:go_router/go_router.dart';
+
+final GoRouter _router = GoRouter(
+  routes: [
+    // Define your routes here
+  ],
+);
+GoRouter createRouter() => _router;
